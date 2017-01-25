5a6b2e1466e502027932d592e652e8cd837ca365
franci98 <franciklavz@gmail.com>
2017-01-25 17:41:01 +0100

    Update EnvironmentValues.py

--- EnvironmentValues.py
+++ EnvironmentValues.py
@@ -9,3 +9,17 @@ import time
 # Some definitions get lat and lon values
 #
 #---------------------------------------------------------*/
+
+
+#-------------------------------------------------------
+# DOBI lokacijo v obliki lat pa lon
+# VRNE napoved kot string da jo jasper direkt pove
+# Link do navodil https://github.com/zejn/arsoapi
+#---------------------------------------------------------*/
+def getVreme(lat, lon):
+  napoved="Vreme bo..."
+  
+  
+  
+  
+  return napoved
