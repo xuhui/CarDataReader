66459abe8333fe005aec61ecb78ac1650faf2c46
franci98 <franciklavz@gmail.com>
2017-01-25 23:20:13 +0100

    Promet funkcija dela
    Najde dogodke v bližini in ijh zloži v array

--- EnvironmentValues.py
+++ EnvironmentValues.py
@@ -1,6 +1,7 @@
 import time
-
-
+import urllib.request
+from haversine import haversine
+import json
 
 #-------------------------------------------------------
 #
@@ -23,3 +24,30 @@ def getVreme(lat, lon):
   
   
   return napoved
+
+#-------------------------------------------------------
+# DOBI lokacijo v obliki lat pa lon
+# VRNE bližnje dogodke kot array da jasper direkt pove
+# Link do navodil https://github.com/zejn/arsoapi
+#---------------------------------------------------------*/
+def getPromet(lat, lon):
+
+    promet = []
+    range = 20 #km
+    currentLocation = (lat, lon)
+    link = "http://janliber.spletniki.si/"
+    webPage = urllib.request.urlopen(link)
+    content = webPage.read()
+
+
+    events = json.loads(content.decode("utf-8"))
+    for event in events:
+        eventLocation = (event['lat'], event['lon'])
+        distance = haversine(currentLocation, eventLocation)
+        if(distance < range):
+            promet.append(event["description"])
+
+
+    return promet
+
+getPromet(46.4221890,14.9262910)
