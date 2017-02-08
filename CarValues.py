8f22c0679de294d18dfdd637847af84604544154
franci98 <franciklavz@gmail.com>
2017-02-08 17:37:11 +0100

    fix

--- CarValues.py
+++ CarValues.py
@@ -8,11 +8,14 @@ import json
 #
 #---------------------------------------------------------
 
+def rpmFetchUnreadOverloads(lastReadTime):
     filepath = "/home/pi/jasper-client/client/car_log/log_rpm.txt"
     file = open(filepath, "r")
 
     rpmLogs = json.loads(file.decode())
 
+    lastLog = rpmLogs[rpmLogs.length - 1]
 
     if lastLog['timestamp'] > lastReadTime:
+        return lastLog
     return None
