d1161607b4016f592d5962e6615b98693b9ae89c
franci98 <franciklavz@gmail.com>
2017-02-08 17:34:47 +0100

    CarValues

--- CarValues.py
+++ CarValues.py
@@ -1,8 +1,18 @@
 import time
+import json
 
 #-------------------------------------------------------
 #
 # Gets values from OBD II connector
 #
 #
-#---------------------------------------------------------*/
+#---------------------------------------------------------
+
+    filepath = "/home/pi/jasper-client/client/car_log/log_rpm.txt"
+    file = open(filepath, "r")
+
+    rpmLogs = json.loads(file.decode())
+
+
+    if lastLog['timestamp'] > lastReadTime:
+    return None
